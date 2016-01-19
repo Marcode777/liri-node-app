214b472529b94c156752b4345dd906217d71dd16
Marcode777 <marc102202@hotmail.com>
2016-01-19 14:42:32 -0500

    modified liri.js with commands

--- liri.js
+++ liri.js
@@ -1,1 +1,24 @@
-var keys = require('keys.js')
+var keys = require('./keys.js');
+  if process.argv[2]==='my-tweets'){
+    console.log('my-tweets');
+  } else if(process.argv[2] === 'spotify-this-song'){
+    console.log('spotify-this-song');
+  } else if(process.argv[2] === 'movie-this'){
+    console.log('spotify-this-song');
+  } else if (process.argv[2]==='do-what-it-says'){
+    console.log('do-what-it-says');
+  };
+
+  //console.log("trying to do the hw");
+  //function my-tweets(){
+    //console.log("something")
+  //};
+  //function spotify-this-song(){
+    //console.log("something 2")
+  //}
+  //function movie-this(){
+    //console.log("something 3")
+  //}
+  //function do-what-it-says(){
+    //console.log("something 4")
+  //}
