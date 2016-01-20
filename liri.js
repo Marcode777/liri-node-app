1bcd797086d141b90c2880eb2c26a1b772d2a4a7
Marcode777 <marc102202@hotmail.com>
2016-01-19 21:42:19 -0500

    got the code to retrieve tweets haha

--- liri.js
+++ liri.js
@@ -1,15 +1,26 @@
 var keys = require('./keys.js');
+var Twitter = require('twitter');
   if (process.argv[2]==='my-tweets'){
-    console.log('my-tweets');
-    console.log(keys.twitterKeys);
+    console.log('my-tweets'); 
+    //console.log(keys.twitterKeys);
     var client = new Twitter(keys.twitterKeys);
+    var params = {q: 'MarcodeTriple7'};
+    client.get('search/tweets',params,function(error, tweets, response){      
+      if(!error) {
+        console.log(tweets);
+      } else {
+        console.log(error)
+      }
+      //console.log(response);
+    });
 } else if(process.argv[2] === 'spotify-this-song'){
     console.log('spotify-this-song');
   } else if(process.argv[2] === 'movie-this'){
-    console.log('spotify-this-song');
+    console.log('movie-this');
   } else if (process.argv[2]==='do-what-it-says'){
     console.log('do-what-it-says');
   };
+
 
   //console.log("trying to do the hw");
   //function my-tweets(){
