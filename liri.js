d9a2de4cd303352106f949d5cef2530117238757
Marcode777 <marc102202@hotmail.com>
2016-01-21 17:38:49 -0500

    got spotify to work

--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
 var keys = require('./keys.js');
 var Twitter = require('twitter');
+var spotify = require('spotify');
+var params = process.argv;
+var selection = params[3];
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});;
   if (process.argv[2]==='my-tweets'){
     console.log('my-tweets'); 
     //console.log(keys.twitterKeys);
@@ -15,6 +27,15 @@ var Twitter = require('twitter');
     });
 } else if(process.argv[2] === 'spotify-this-song'){
     console.log('spotify-this-song');
+    spotify.search({ type: 'track', query: selection }, function(err, data) {
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+    } 
+      console.log(data);
+ 
+    // Do something with 'data' 
+});
   } else if(process.argv[2] === 'movie-this'){
     console.log('movie-this');
   } else if (process.argv[2]==='do-what-it-says'){
